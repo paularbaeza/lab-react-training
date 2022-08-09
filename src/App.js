3879256c723a29c3ecc470eaf47bbd9a4529da5e
Paula Rodríguez <paularb923@hotmail.com>
2022-08-09 16:39:13 +0100

    iteration 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
+import BoxColor from './components/BoxColor';
 
 function App() {
   return (
@@ -14,7 +15,6 @@ function App() {
         birth={new Date('1992-07-14')}
         picture="https://randomuser.me/api/portraits/men/44.jpg"
       />
-
       <IdCard
         lastName="Delores "
         firstName="Obrien"
@@ -23,12 +23,16 @@ function App() {
         birth={new Date('1988-05-11')}
         picture="https://randomuser.me/api/portraits/women/44.jpg"
       />
-
+      <hr />
       <Greetings lang="de">Ludwig</Greetings>
       <Greetings lang="fr">François</Greetings>
-
+      <hr />
       <Random min={1} max={6} />
       <Random min={1} max={100} />
+      <hr />
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
---
+++ src/components/BoxColor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BoxColor(props) {
+const {r, g, b} = props
+  return (
+    <div style={{backgroundColor: `rgb(${r},${g},${b})`, height: 80 }}>
+    <p>rgb({r},{g},{b})</p>
+    </div>
+  )
+}
+
+export default BoxColor
